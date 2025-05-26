0f63470f7221719ef0620a9e927e0adebca6979b
Pri-Nigam <priyanshunigam1607@gmail.com>
2025-05-26 12:31:52 +0530

    Changed app into api only

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ReactPlusRails
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.api_only = true
 
     # Configuration for the application, engines, and railties goes here.
     #
